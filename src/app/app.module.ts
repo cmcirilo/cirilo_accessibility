246ab08068207ec94fd6b6ab87506b645440f414
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-06-14 12:30:16 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, YesNoButtonGroupModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yes-no-button-group',
+  templateUrl: './yes-no-button-group.component.html',
+  styleUrls: ['./yes-no-button-group.component.scss'],
+})
+export class YesNoButtonGroupComponent implements OnInit {
+  @Input() public value: string = null;
+  @Input() public label = '';
+  public options = YesNoButtonGroupOptions;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public activate(value: string) {
+    this.value = value;
+  }
+}
+
+enum YesNoButtonGroupOptions {
+  YES = 'yes',
+  NO = 'no',
+}
