1b9318aa84c553af6465a730ac5c256a0ee0bf04
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-07-01 16:50:56 -0300

    adjusting names

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'a11y-p1'`, () => {
+  it(`should have as title 'cirilo_accessibility'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('a11y-p1');
+    expect(app.title).toEqual('cirilo_accessibility');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('a11y-p1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'cirilo_accessibility app is running!'
+    );
   });
 });
