ee4a67089dc47deac30b0b711f3ba16c508d0bf5
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-07-10 19:12:54 -0300

    creating keyboard manager item directive

--- src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -1,13 +1,23 @@
-import { Directive, HostListener } from '@angular/core';
+import {
+  ContentChildren,
+  Directive,
+  HostListener,
+  QueryList,
+} from '@angular/core';
+import { KeyboardManagerItemDirective } from './keyboard-manager-item.directive';
 
 @Directive({
   selector: '[appKm]',
 })
 export class KeyboardManagerDirective {
+  @ContentChildren(KeyboardManagerItemDirective)
+  public items: QueryList<KeyboardManagerItemDirective> = null;
+
   @HostListener('keyup', ['$event'])
   public manageKeys(event: KeyboardEvent): void {
     switch (event.key) {
       case 'ArrowUp':
+        console.log(this.items);
         console.log('up');
         break;
       case 'ArrowDown':
--- src/app/shared/directives/keyboard-manager/keyboard-manager.module.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { KeyboardManagerItemDirective } from './keyboard-manager-item.directive';
 import { KeyboardManagerDirective } from './keyboard-manager.directive';
 
 @NgModule({
-  declarations: [KeyboardManagerDirective],
+  declarations: [KeyboardManagerDirective, KeyboardManagerItemDirective],
   imports: [CommonModule],
-  exports: [KeyboardManagerDirective],
+  exports: [KeyboardManagerDirective, KeyboardManagerItemDirective],
 })
 export class KeyboardManagerModule {}
