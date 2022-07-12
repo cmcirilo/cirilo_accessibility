0f1f5e27760d7bbce69b47631c3fb4322f5c9cc1
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-07-12 12:00:33 -0300

    refactoring yes-no-button-group template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
+import { DisableControlModule } from './shared/directives/disable-control/disable-control.module';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, YesNoButtonGroupModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    YesNoButtonGroupModule,
+    ReactiveFormsModule,
+    DisableControlModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
